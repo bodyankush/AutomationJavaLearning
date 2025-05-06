7cb2922526c76c883c167a14af070d3e48f9384d
Bohdan Holovko QA Automation <holovkobusiness@gmail.com>
2025-05-06 13:37:00 +0300

    refactor: added spaces before and after methods in SearchResultsPage class

--- src/main/java/pages/SearchResultsPage.java
+++ src/main/java/pages/SearchResultsPage.java
@@ -9,25 +9,28 @@ import static com.codeborne.selenide.CollectionCondition.size;
 
 
 public class SearchResultsPage {
-
     public static class HeaderComponent {
         private static final SelenideElement pageTitle = $x("//div[@class='page-title']//h1[@class='t2']");
+
         public static void shouldHaveTitle (String expectedTitle) {
             pageTitle.shouldHave(text(expectedTitle));
         }
-    }
 
+    }
     public static class ListOfItemsComponent {
         private static final ElementsCollection amountOfItems = $$x("//div[contains(@class, 'model-short-div list-item--goods')]");
+
         public static void ShouldHaveItemsCount (int expectedItemsCount) {
             amountOfItems.shouldHave(size(expectedItemsCount));
         }
-    }
 
+    }
     public static class PaginationComponent {
         private static final SelenideElement lastPaginationButton = $x("(//div[@class='ib page-num']//a[text() and not(@id='pager_dots')])[last()]");
+
         public static void ShouldHavePaginationCountMoreThan(int minPageCount){
             int actualCount = Integer.parseInt(lastPaginationButton.getText());
         }
+
     }
 }
